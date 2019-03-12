f45f248f0e3f57b0cf24f0d1ddc4fa114e2210ea
Madhu <madhubalan432@gmail.com>
2019-03-12 17:14:27 +0530

    Submit the data to the node server

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  uri = 'http://localhost:4000/post';
+
+  constructor(private http: HttpClient) { }
+
+  addPost(title,description) {
+    const obj = {
+      title: title,
+      description: description
+    };
+    console.log(obj);
+    this.http.post(`${this.uri}/add`, obj)
+        .subscribe(res => console.log('Done'));
+  }
 }
