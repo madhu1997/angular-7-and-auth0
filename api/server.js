24c60e9e78d7a7e35471130e102d422758081bc3
Madhu <madhubalan432@gmail.com>
2019-03-18 11:16:56 +0530

    tried for access token integrated

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const jwt = require('express-jwt');
 const jwtAuthz = require('express-jwt-authz');
+const request = require("request");
 const jwksRsa = require('jwks-rsa'),
     path = require('path'),
     bodyParser = require('body-parser'),
@@ -27,6 +28,53 @@ const jwksRsa = require('jwks-rsa'),
     req.postId = req.params.postId;
     next()
   }
+  
+ /* app.get('/api/public', function(req, res) {
+    res.json({
+      message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
+    });
+  });
+  app.get('/api/private', checkJwt, function(req, res) {
+    res.json({
+      message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+    });
+  });
+
+  const checkScopes = jwtAuthz([ 'read:users' ]);
+
+  app.get('/api/private-scoped', checkJwt, checkScopes, function(req, res) {
+  res.json({
+    message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
+  });
+});*/
+
+/*var options = { method: 'POST',
+  url: 'https://dev-wyw2s199.auth0.com/oauth/token',
+  headers: { 'content-type': 'application/json' },
+  body: 
+   { 
+     grant_type: 'client_credentials',
+     client_id: 'FM8ECs5V8C5ETJpXqqzxF78WNBe612Dl',
+     client_secret: 'VTFIDmRxrZPl8h1Zt0H_mYrP4WRV2ITk0Vb0xUsDOYvhQyyJ3jKX_igb4Z8FpdC4',
+     audience: 'https://localhost:4200' 
+    },
+  json: true };
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+  
+    console.log(body);
+  });*/
+
+  const handleUser = function(req, res, next) {
+   // console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+    if (req.headers.authorization) {
+      var token = req.headers.authorization.split(" ")[1];
+    
+      console.log(token);
+    }
+    //console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+    next();
+  }
   const checkJwt = jwt({
     // Dynamically provide a signing key
     // based on the kid in the header and 
@@ -44,16 +92,21 @@ const jwksRsa = require('jwks-rsa'),
     algorithms: ['RS256']
   });
 
-  const handleUser = function(req, res, next) {
-    console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
-    if (req.headers.authorization) {
-      var token = req.headers.authorization.split(" ")[1];
-    
-      console.log(token);
-    }
-    console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
-    next();
-  }
+
+  app.get('api/users', checkJwt, (req, res) => {
+    let userIds = getUsersFromDB(); //Array of User IDs from MongoDB for example (to be used later)
+    console.log('Auth0 Access Token', req.auth0AccessToken);
+});
+
+/*let getUser = (accessToken, userid) => {
+  return requestPromise({
+      url: `https://dev-wyw2s199.auth0.com/api/v2/users/${userid}`,
+      headers: {
+          'authorization': `Bearer ${accessToken}`
+      }
+  });
+}*/
+
     app.use('/post', handleUser, postRoute);
     app.use('/post/:postId/comment',getPost, commentRoute);
     //app.use('/user',userRoute);
